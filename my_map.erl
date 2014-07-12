2765d1040edafb2ebf97dedf289e5a93f20f2adf
Clark Kampfe <clark@bellycard.com>
2014-07-12 18:05:26 -0500

    refactored

--- my_map.erl
+++ my_map.erl
@@ -1,42 +1,22 @@
 -module(my_map).
 -export([pmap/2]).
 
-%% parallel #map
-%% currently: one process per list element
-%% this is not efficient, consider sharding
-%% the list into chunks based on size,
-%% with each process consuming a fraction of the total
-%% list
+%% parallel map
+%% results are indeterminably ordered
 
 pmap(Function, List) ->
-    pmap(self(), Function, List),
+    Parent = self(),
+    Pids = lists:map(
+             fun(El) ->
+                  spawn_link(fun() -> Parent ! {ok, catch(Function(El))} end)
+             end,
+             List
+           ),
+    gather(length(Pids), []).
+
+gather(0, ResultsList) ->
+    lists:reverse(ResultsList);
+gather(RemainingCount, ResultsList) ->
     receive
-        {ok, Results} -> lists:reverse(Results)
-    end.
-pmap(MainPid, Function, List) ->
-
-    %% spawn results aggregator
-    ResultsPid = spawn(fun() -> result_aggregator(MainPid, length(List), []) end),
-
-    %% worker
-    ItemWorker = fun(Item) ->
-                     Result = Function(Item),
-                     ResultsPid ! {ok, Result}
-                 end,
-
-    %% foreach
-    [spawn(fun() -> ItemWorker(Item) end) || Item <- List],
-    ok.
-
-
-%% aggregate results
-result_aggregator(MainPid, RemainingCount, ResultsList) ->
-    receive
-        {ok, Result} ->
-            case RemainingCount of
-                N when N =:= 1 ->
-                    MainPid ! {ok, [Result|ResultsList]};
-                _ ->
-                    result_aggregator(MainPid, RemainingCount - 1, [Result|ResultsList])
-            end
+        {ok, Result} -> gather(RemainingCount - 1, [Result|ResultsList])
     end.
