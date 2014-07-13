8ef6d6bbc93a603bc6ba3a230ea24f57e73f941c
Clark Kampfe <clark@bellycard.com>
2014-07-12 19:29:01 -0500

    pfilter

--- pfuns.erl
+++ pfuns.erl
@@ -1,7 +1,8 @@
 -module(pfuns).
 -export([
          pmap/2, %% parallel map
-         bmark/1 %% benchmark
+         bmark/1, %% benchmark
+         pfilter/2
         ]).
 
 
@@ -37,3 +38,29 @@ bmark(ListOfFns, ListOfResults) ->
     F(),
     {H2,M2,S2} = time(),
     bmark(RemainingFns, [{H2-H1, M2-M1, S2-S1} | ListOfResults]).
+
+
+%% parallel filter
+%% results are indeterminably ordered
+pfilter(Function, List) ->
+    Parent = self(),
+    Pids = lists:map(
+             fun(El) ->
+                  spawn_link(fun() ->
+                       case Function(El) of
+                           true -> Parent ! {ok, El};
+                           _    -> Parent ! {false}
+                       end
+                  end)
+             end,
+             List
+           ),
+    gather_pfilter(length(Pids), []).
+
+gather_pfilter(0, ResultsList) ->
+    lists:reverse(ResultsList);
+gather_pfilter(RemainingCount, ResultsList) ->
+    receive
+        {ok, Result} -> gather_pfilter(RemainingCount - 1, [Result|ResultsList]);
+        {false}      -> gather_pfilter(RemainingCount - 1, ResultsList)
+    end.
